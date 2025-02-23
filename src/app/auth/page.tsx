a1f8850590561a1610ced8b73d152a4b94ac0149
Danilo Augusto Correia da Silva <dacs2012@gmail.com>
2025-02-23 02:23:14 -0500

    idk

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,6 +2,19 @@
 import React from "react";
 import { Button, Card } from "@heroui/react";
 import { signOut, useSession } from "next-auth/react";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig;
 
 const fakeJobs = [
   {
@@ -30,6 +43,15 @@ const fakeJobs = [
   },
 ];
 
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
 const Dashboard = () => {
   const session = useSession();
 
@@ -39,6 +61,21 @@ const Dashboard = () => {
         <h1 className="text-2xl font-bold">Applicants Dashboard</h1>
       </header>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <ChartContainer config={chartConfig} className="h-[200px] w-full">
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
         {fakeJobs.map((job) => (
           <Card key={job.id} className="p-4 shadow rounded-lg">
             <h2 className="text-xl font-semibold mb-2">{job.title}</h2>
