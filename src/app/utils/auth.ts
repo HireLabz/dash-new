b99b14dc90dd92fe1a6d4b4c5b172a637797ca87
Danilo Augusto Correia da Silva <dacs2012@gmail.com>
2025-02-23 08:23:47 -0500

    restrict email access

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,11 +1,30 @@
-import { SessionStrategy } from "next-auth";
+import { Account, Profile, SessionStrategy, User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
 import Google from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
-      Google({
-          clientId: process.env.GOOGLE_ID ?? "",
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      }),
+    Google({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
   ],
+  callbacks: {
+    async signIn({
+      user,
+      account,
+      profile,
+      email,
+      credentials,
+    }: {
+      user: AdapterUser | User;
+      account: Account | null;
+      profile?: Profile;
+      email?: { verificationRequest?: boolean };
+      credentials?: Record<string, any>;
+    }): Promise<boolean> {
+      const allowedEmails = ["dacs2012@gmail.com", "itamardprf@gmail.com"];
+      return allowedEmails.includes(email?.verificationRequest ? "" : (user.email ?? "")) || false;
+    },
+  },
 };
